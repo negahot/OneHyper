d84efa4564aa959db29a1d25fb5a1187e3628b65
negahot <clashroyale1000gem@gmail.com>
2016-08-28 15:02:48 +0430

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -62,7 +62,7 @@ _ساخته شده با ❤️ در 🇮🇷_
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {94746365}-- put your id here
+  local admins = {253364622}-- put your id here
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
